f9e579897a5c758ed1251cd21bbfcb089c74e906
Austin Jones <austin.jones@uster.com>
2024-07-18 10:15:25 -0400

    shorter

--- tctcto.py
+++ tctcto.py
@@ -1,21 +1,17 @@
 r = range
 def cr(b): return [b[i*3:(i+1)*3] for i in r(3)]
 def cc(b): return [b[i::3] for i in r(3)]
-def cd(b): return [[b[0], b[4], b[8]], [b[2], b[4], b[6]]]
+def cd(b): return [b[::4], b[2:7:2]]
 def leq(ri, t=False): return [t:=t or (len(set(r))==1 and r[0]!=' ') for r in ri][-1]
 def pl(t): return "OX"[t%2]
-def pr(b): [print(r) for r in cr(b)]
+def pr(b): return '\n'.join(str(r) for r in cr(b))
 
 bd, t = [' '] * 9, 0
-
-pr(bd)
-while (n := input()):
+while (n := input("\n{}\nEnter move for {}\n".format(pr(bd),pl(t:=t+1)))):
     m = int(n)
     if m in [i for i,s in enumerate(bd) if s == ' ']:
         bd[m] = pl(t)
-        pr(bd)
         if leq(cr(bd) + cc(bd) + cd(bd)):
             break;
-        t+=1
 
 print(pl(t) + " wins!")
